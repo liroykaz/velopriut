f9c30f47865f9a88b2d543c8c0490faa740277a9
Pavel <liroy63@mail.ru>
2018-06-01 16:39:03 +0400

    dev

--- modules/web/src/com/company/salescafe/web/product/ProductBrowse.java
+++ modules/web/src/com/company/salescafe/web/product/ProductBrowse.java
@@ -1,6 +1,23 @@
 package com.company.salescafe.web.product;
 
+import com.company.salescafe.entity.Product;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.data.GroupDatasource;
+
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.UUID;
 
 public class ProductBrowse extends AbstractLookup {
+    @Inject
+    GroupDatasource<Product, UUID> productsDs;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        productsDs.addItemPropertyChangeListener(e->{
+            if("isAvailable".equals(e.getProperty()))
+                productsDs.commit();
+        });
+    }
 }
